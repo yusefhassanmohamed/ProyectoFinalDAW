99f3b67ed298515d1fe9c743ee0b2385d1c376ba
Yusef <yusefhassanmohamedserio@gmail.com>
2021-11-25 12:34:51 +0100

    Actualizacion cambio DB

--- Codigo/handyman/libs/database.php
+++ Codigo/handyman/libs/database.php
@@ -2,7 +2,7 @@
     class Conectar{
         
         public static function conexion(){
-            $conexion = new mysqli("192.168.0.113", "root", "root", "handyman", 3306);
+            $conexion = new mysqli("192.168.0.16", "root", "", "handyman");
             return $conexion;
         }
     }
