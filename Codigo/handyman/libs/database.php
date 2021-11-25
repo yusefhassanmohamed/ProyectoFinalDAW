4763df44125eea2d26fc889ba64fce0e5a5f7a8f
Yusef <yusefhassanmohamedserio@gmail.com>
2021-11-25 15:40:12 +0100

    Actualizacion

--- Codigo/handyman/libs/database.php
+++ Codigo/handyman/libs/database.php
@@ -2,7 +2,7 @@
     class Conectar{
         
         public static function conexion(){
-            $conexion = new mysqli("127.0.0.1", "root", "", "handyman", 3306);
+            $conexion = new mysqli("127.0.0.1", "debianDB", "debianDB", "handyman", 3306);
             return $conexion;
         }
     }
