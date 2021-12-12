70dd7d0b93d3191d3540c8e0ca3f07e3f3a49598
Yusef <yusefhassanmohamedserio@gmail.com>
2021-12-12 19:02:28 +0100

    Actualizacion validacion

--- Codigo/handyman/libs/database.php
+++ Codigo/handyman/libs/database.php
@@ -1,7 +1,7 @@
 <?php
     class Conectar{
         public static function conexion(){
-            $conexion = new mysqli("127.0.0.1", "debianDB", "debianDB", "handyman", 3306);
+            $conexion = new mysqli("127.0.0.1", "root", "", "handyman", 3306);
             return $conexion;
         }
     }
